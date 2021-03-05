2f530b59536b47c221639afe5d3c4a9f603c7af4
mbalcer <mateusz75319@gmail.com>
2021-03-05 19:54:15 +0100

    Initialize project

---
+++ src/main/java/pl/edu/utp/pz1/Pz1Application.java
@@ -0,0 +1,13 @@
+package pl.edu.utp.pz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pz1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pz1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/utp/pz1/Pz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.utp.pz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
