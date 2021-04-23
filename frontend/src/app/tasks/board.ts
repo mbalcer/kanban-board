52a4fbed72bad1e968c0f8797f016be47f5ac688
mbalcer <mateusz75319@gmail.com>
2021-04-23 21:20:26 +0200

    Created more lists in tasks board

---
+++ frontend/src/app/tasks/board.ts
@@ -0,0 +1,6 @@
+import {Task} from './task';
+
+export interface Board {
+  name: string;
+  tasks: Task[];
+}
--- frontend/src/app/tasks/tasks.component.ts
+++ frontend/src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from './task';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {Board} from './board';
 
 @Component({
   selector: 'app-tasks',
@@ -8,16 +9,24 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  todo: Task[] = [];
-  done: Task[] = [];
+  boards: Board[] = [];
 
   constructor() {
-    this.todo.push({id: 1, name: 'task', description: 'description', createDateTime: '2021-04-22'});
-    this.todo.push({id: 2, name: 'new_task', description: 'description', createDateTime: '2021-04-21'});
-    this.done.push({id: 3, name: 'old_task', description: 'description', createDateTime: '2021-04-22'});
+    this.initBoards();
+    this.boards[0].tasks.push({id: 1, name: 'task', description: 'description', createDateTime: '2021-04-22'});
+    this.boards[0].tasks.push({id: 2, name: 'new_task', description: 'description', createDateTime: '2021-04-21'});
+    this.boards[0].tasks.push({id: 3, name: 'old_task', description: 'description', createDateTime: '2021-04-22'});
   }
 
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  initBoards() {
+    this.boards.push({name: 'Do zrobienia', tasks: []});
+    this.boards.push({name: 'W trakcie', tasks: []});
+    this.boards.push({name: 'Testowanie', tasks: []});
+    this.boards.push({name: 'Zrobione', tasks: []});
   }
 
   // tslint:disable-next-line:typedef
