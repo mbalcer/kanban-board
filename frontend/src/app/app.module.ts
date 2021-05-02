46bcdc7cd45304fb3669813eb6a2eae5c5893877
mbalcer <mateusz75319@gmail.com>
2021-05-02 19:42:38 +0200

    Added dialog displaying task details

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,20 +7,25 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {DialogTaskDetails} from './tasks/dialog-task-details/dialog-task-details';
+import {MaterialModule} from './material-module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    TasksComponent,
+    DialogTaskDetails
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     DragDropModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DialogTaskDetails]
 })
 export class AppModule { }
