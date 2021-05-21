ccc284a0ada11ea6a5f7190ff354cff07241a3c6
mbalcer <mateusz75319@gmail.com>
2021-05-21 13:56:12 +0200

    Initiated connecting to chat

--- frontend/src/app/tasks/chat/chat.component.ts
+++ frontend/src/app/tasks/chat/chat.component.ts
@@ -1,15 +1,62 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import {environment} from '../../../environments/environment';
+import {Project} from '../../home/projects/project';
+import {Student} from '../../auth/student/student.model';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnChanges, OnDestroy {
+  private serverUrl = environment.backendUrl + '/chat';
+  private stompClient;
 
-  constructor() { }
+  message: string;
+  initWebSocket = false;
 
-  ngOnInit(): void {
+  @Input() project: Project;
+  @Input() user: Student;
+
+  constructor() {
+
   }
 
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    if (this.project && !this.initWebSocket) {
+      this.webSocketConnect(this.project.projectId);
+      this.initWebSocket = true;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.webSocketDisconnect(this.project.projectId);
+  }
+
+  webSocketConnect(projectId): void {
+    const ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect({}, frame => {
+      this.stompClient.subscribe('/project/' + projectId, message => {
+        console.log(message);
+      });
+    });
+  }
+
+  webSocketDisconnect(projectId): void {
+    this.stompClient.disconnect('/project/' + projectId);
+  }
+
+  sendMessage(): void {
+    const messageToSend = {
+      student: this.user,
+      message: this.message,
+    };
+    this.stompClient.send('/app/chat/' + this.project.projectId, {}, JSON.stringify(messageToSend));
+    this.message = '';
+  }
 }
