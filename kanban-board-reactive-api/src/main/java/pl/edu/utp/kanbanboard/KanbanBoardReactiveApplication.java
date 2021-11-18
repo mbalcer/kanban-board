bdfe7db7a6ce48b5adc9c836dbd430692a2b0875
mbalcer <mateusz75319@gmail.com>
2021-11-18 22:01:19 +0100

    Initialize reactive api microservice

---
+++ kanban-board-reactive-api/src/main/java/pl/edu/utp/kanbanboard/KanbanBoardReactiveApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.utp.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanBoardReactiveApplication.class, args);
+    }
+
+}
---
+++ kanban-board-reactive-api/src/test/java/pl/edu/utp/kanbanboard/KanbanBoardReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.utp.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
