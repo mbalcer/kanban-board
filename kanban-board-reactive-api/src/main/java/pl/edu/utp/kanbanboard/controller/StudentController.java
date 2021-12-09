487d978cd3a843e53c39dbbff0eb693bd5b32b76
mbalcer <mateusz75319@gmail.com>
2021-12-09 22:27:11 +0100

    Added ResponseEntity to response in Student API

--- kanban-board-reactive-api/src/main/java/pl/edu/utp/kanbanboard/controller/StudentController.java
+++ kanban-board-reactive-api/src/main/java/pl/edu/utp/kanbanboard/controller/StudentController.java
@@ -1,11 +1,14 @@
 package pl.edu.utp.kanbanboard.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.utp.kanbanboard.model.Student;
 import pl.edu.utp.kanbanboard.service.StudentService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/api/student", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -22,27 +25,39 @@ public class StudentController {
     }
 
     @GetMapping("/{id}")
-    public Mono<Student> getStudentById(@PathVariable String id) {
-        return studentService.get(id);
+    public Mono<ResponseEntity<Student>> getStudentById(@PathVariable String id) {
+        return studentService.get(id)
+                .map(student -> ResponseEntity.ok(student))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
     @GetMapping("/email/{email}")
-    public Mono<Student> getStudentByEmail(@PathVariable String email) {
-        return studentService.getByEmail(email);
+    public Mono<ResponseEntity<Student>> getStudentByEmail(@PathVariable String email) {
+        return studentService.getByEmail(email)
+                .map(student -> ResponseEntity.ok(student))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
     @PostMapping
-    public Mono<Student> createStudent(@RequestBody Student student) {
-        return studentService.create(student);
+    public Mono<ResponseEntity<Student>> createStudent(@RequestBody Student student) {
+        return studentService.create(student)
+                .map(s -> {
+                    URI location = URI.create("/api/student/" + s.getStudentId());
+                    return ResponseEntity.created(location).body(student);
+                });
     }
 
     @PutMapping("/{id}")
-    public Mono<Student> updateStudent(@PathVariable String id, @RequestBody Student student) {
-        return studentService.update(id, student);
+    public Mono<ResponseEntity<Student>> updateStudent(@PathVariable String id, @RequestBody Student student) {
+        return studentService.update(id, student)
+                .map(s -> ResponseEntity.ok(s))
+                .switchIfEmpty(Mono.just(ResponseEntity.badRequest().build()));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable String id) {
-        studentService.delete(id);
+    public Mono<ResponseEntity<Object>> deleteStudent(@PathVariable String id) {
+        return studentService.delete(id)
+                .map(student -> ResponseEntity.noContent().build())
+                .switchIfEmpty(Mono.just(ResponseEntity.badRequest().build()));
     }
 }
